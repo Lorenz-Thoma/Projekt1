ef1f6624d4a02d2e633b60d1218707ee8cd53d53
Lorenz-Thoma <lorenz.thoma@outlook.com>
2025-04-17 10:38:47 +0530

    Meine  Änderungen

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const gallery = document.getElementById('gallery');
-    const button = document.querySelector('.expand-btn');
-    let expanded = false;
+const button = document.querySelector('.expand-btn');
+let expanded = false;
 
-    function toggleGallery() {
-      expanded = !expanded;
-      gallery.classList.toggle('expanded', expanded);
-      button.textContent = expanded ? 'Weniger anzeigen' : 'Erweitern';
-    }
+function toggleGallery() {
+  expanded = !expanded;
+  gallery.classList.toggle('expanded', expanded);
+  button.textContent = expanded ? 'Weniger anzeigen' : 'Erweitern';
+}
