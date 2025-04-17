60b95922070f7868a548724f5f3b25d3175d42a5
Lorenz-Thoma <lorenz.thoma@outlook.com>
2025-04-17 16:13:17 +0530

    Meine  Änderungen

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ function toggleGallery() {
   gallery.classList.toggle('expanded', expanded);
   button.textContent = expanded ? 'Weniger anzeigen' : 'Erweitern';
 }
+
+function sendHeight() {
+  const height = document.body.scrollHeight;
+  window.parent.postMessage({ type: 'setHeight', height }, '*');
+}
+
+window.onload = sendHeight;
+window.addEventListener('resize', sendHeight);
