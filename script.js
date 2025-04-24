208572637f63d03dc6b91335f0195b0c2fb7011a
Lorenz-Thoma <lorenz.thoma@outlook.com>
2025-04-24 15:26:46 +0530

    lo

--- script.js
+++ script.js
@@ -6,12 +6,35 @@ function toggleGallery() {
   expanded = !expanded;
   gallery.classList.toggle('expanded', expanded);
   button.textContent = expanded ? 'Weniger anzeigen' : 'Erweitern';
+
+  sendHeight()
 }
 
-function sendHeight() {
+
+function sendHeight(){
+  const height = document.body.scrollHeight;
+  window.parent.postMessage({ type: 'setHeight', height }, '*');
+}
+
+
+/*function sendHeight() {
   const height = document.body.scrollHeight;
   window.parent.postMessage({ type: 'setHeight', height }, '*');
 }
 
 window.onload = sendHeight;
-window.addEventListener('resize', sendHeight);
+window.addEventListener('resize', sendHeight);*/
+
+// Beispiel: Nachricht an die Hauptseite senden
+
+
+/*window.onload = function () {
+  const message = {
+    type: 'userData',
+    user: 'Anna',
+    age: 22
+  };
+
+  // Nachricht an das übergeordnete Fenster (die Wix-Seite) senden
+  window.parent.postMessage(message, '*'); // Du kannst statt '*' auch die exakte URL der Wix-Seite eintragen
+};*/
